50cb9184344d62d28e5a3fbb05164c2995f1c879
Saif Ali <saifali7646@gmail.com>
2021-07-28 21:02:00 +0000

    final submission

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,60 @@
+
 const wordSearch = (letters, word) => { 
+    //Vertical
+    let outsideArray = [];
+    for( let row in letters[0]){
+        //console.log(letters[row]);
+        let empytArray = [];
+        for(let col in letters){
+            empytArray.push(letters[col][row]);
+        }
+        outsideArray.push(empytArray);
+    }
+    
+    let verticalJoin = outsideArray.map(x => x.join(''));
+
+    for(y of verticalJoin){
+        if(y.includes(word)){
+            return true;
+        }
+    }
+
+    
+    //Horizontal
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)){
+            return true
+        } 
+        
     }
+    return false
 }
 
 module.exports = wordSearch
+
+const result1 = wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ],'LDLGLBISL' );
+  const result2 = wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'SEINFELD');
+
+  console.log(result1);
+  console.log(result2);
